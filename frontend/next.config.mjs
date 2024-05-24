b476687baa5463d37d0ec4b2ab963b1b9de8a2a4
Trevorton27 <spiral272@gmail.com>
2024-05-24 17:57:17 +0900

    had to reconfigure config.js to run without babel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      newNextLinkBehavior: false,
+    env: {
+      API_URL: 'http://localhost:8000',
+    },
+    modularizeImports: {
+      "react-bootstrap": {
+        transform: "react-bootstrap/{{member}}",
+      },
+      lodash: {
+        transform: "lodash/{{member}}",
+      },
     },
 };
 
